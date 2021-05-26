edfc9fa3834a92696d5124c6a8c8f56a7fcd61eb
mcelam00 <mcelam00@estudiantes.unileon.es>
2021-05-26 19:21:33 +0200

    Middleware: Conceptos Básicos

--- index.js
+++ index.js
@@ -7,11 +7,33 @@ const express = require('express');
 const servidor = express(); 
 
 
+
+/*MIDDLEWARE: Aquí irían las funciones y llamadas correspondientes que se ejecutan antes de las particulares que dan respuesta según el metodo y ruta
+    Todo Middleware se ejecuta con la funcion servidor.use(), aunque no tiene que ser una funcion lo que llame a ejecutar el .use()
+    Esta sección sirve para extender el código del servidor
+        -Podríamos por ejemplo procesar datos como autenticar un usuario ya que podemos tener los tres parámetros para acceder al cuerpo y la url en la funcion igualmente
+        -Muchos middleware ya existen como el express.json() y no hay que hacerlos nosotros explicitamente
+        -puedo requerirlos arriba (a parte de los de express.) podré requerir los de lo que importe arriba
+*/
+
+function ejemploMiddleware(request, response, next){
+    console.log('Probando el Middleware, a ver si me pinto por el medio');
+    console.log(`CONEXION ENTRANTE:\n   +Protocolo ->${request.protocol}\n  +HOST ->${request.get('host')}\n    +RUTA PEDIDA ->${request.originalUrl}`)
+    next(); //para que continue la ejecución
+}
+
+
+
 //Cabecera para entender los json que me llegan al servidor
-servidor.use(express.json());
+servidor.use(express.json()); //es un middleware que transforma los objetos a .json para que podamos entenderlo despues
+
+servidor.use(ejemploMiddleware);
 
 
 
+
+
+/*Todo el rato estamos pasando funciones anónimas por parámetro que definimos implicitamente en la misma signatura de la llamada*/
 //listener genérico independiente del tipo de método que se use en la peticion
 servidor.all('/', (request, response, next) =>{
 
